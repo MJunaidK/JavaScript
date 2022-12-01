1d5971b9ccdf2d2ddf904b1510f2d0023c394e70
MJunaidK <junaidcts@gmail.com>
2022-11-30 20:40:45 -0500

    Adding variables and values

---
+++ 01-Fundamentals-Part-1/Value And Variables/script.js
@@ -0,0 +1,18 @@
+let firstName="Junaid";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let junaid_khan= "JK";
+let $function = 27;
+
+let person = "Junaid";
+let PI = 3.14;
+let myFirstJob ="Teacher";
+let myCurrentJob="Programmer";
+
+let job1 = "Teacher";
+let job2= "Programmer";
+
+console.log(myCurrentJob);
