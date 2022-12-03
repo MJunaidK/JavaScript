4f21b02a26d76d2b338b42b34fcbd9f42035db4b
MJunaidK <junaidcts@gmail.com>
2022-12-03 14:34:03 -0500

    Adding String and template literals

---
+++ 01-Fundamentals-Part-1/Lecture 17 String and Template Literals/script.js
@@ -0,0 +1,24 @@
+const firstName = "Junaid";
+const job = 'Architect';
+const birthYear = 1984;
+const year = 2022;
+
+const junaid = "I'm " + firstName + ', a ' +  (year - birthYear) + ' years old ' + job + '!';
+console.log(junaid);
+
+const junaidNew = `I'm ${firstName}, a ${year-birthYear} years old ${job}!`;
+console.log(junaidNew);
+
+console.log(`Just a regular string...`)
+
+console.log('String with \n\
+multiple \n\
+lines \n\
+using single quotes');
+
+
+console.log(`String with 
+multiple 
+lines 
+using back ticks`);
+
