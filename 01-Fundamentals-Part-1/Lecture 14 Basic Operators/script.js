e58efe95fc436b6debbceefe07e00b0d8eb3e772
MJunaidK <junaidcts@gmail.com>
2022-12-02 21:58:10 -0500

    Basic Operatos

---
+++ 01-Fundamentals-Part-1/Lecture 14 Basic Operators/script.js
@@ -0,0 +1,31 @@
+
+// Math Operators
+const now = 2066;
+const ageJunaid = now -1984;
+const ageSarah = now -1995;
+console.log(ageJunaid, ageSarah);
+
+console.log(ageJunaid * 2, ageJunaid/10, 2**3);
+// 2 ** 3  means 2 to the power 3 = 2* 2* 2
+
+const  firstName = "Junaid";
+const lastName = "Khan";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 =25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1;
+x--;
+console.log(x);
+
+// Comparison Operators
+
+console.log(ageJunaid > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >=18;
+
+console.log(now - 1984 > now- 1995);
