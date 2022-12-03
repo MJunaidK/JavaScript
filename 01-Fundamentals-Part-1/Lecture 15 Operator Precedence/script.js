faf047333ee35cc45d9ddac20ce38770b8e8299e
MJunaidK <junaidcts@gmail.com>
2022-12-03 11:35:09 -0500

    Operators Precedence

---
+++ 01-Fundamentals-Part-1/Lecture 15 Operator Precedence/script.js
@@ -0,0 +1,12 @@
+const now = 2066;
+const ageJunaid = now -1984;
+const ageSarah = now -1995;
+
+console.log(now - 1984 > now- 1995);
+
+let x,y;
+x = y = 25 - 10 -5 // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJunaid + ageSarah) / 2;
+console.log(ageJunaid, ageSarah, averageAge);
