3aa00f7274c215849bfb4918b912357cec3b1311
MJunaidK <junaidcts@gmail.com>
2022-12-04 15:23:37 -0500

    Logical Operators

---
+++ 01-Fundamentals-Part-1/Lecture 24 Logical Operators/script.js
@@ -0,0 +1,24 @@
+const hasDriversLicense = true // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if(hasDriversLicense && hasGoodVision){
+    console.log(`Sarah is able to drive!`);
+}else{
+    console.log(`Someone else should drive!`);
+}
+
+const isTired = true;
+console.log(hasDriversLicense || hasGoodVision || isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log(`Alvia is good to drive!`);
+}else{
+    console.log(`Let Aliva's father drive..`);
+}
+
