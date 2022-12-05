2d8dfb4cb1db2d07faed1174dbe940b5b2f5af85
MJunaidK <junaidcts@gmail.com>
2022-12-04 20:07:47 -0500

    Statements and Expressons

---
+++ 01-Fundamentals-Part-1/Lecture 27 Statements and Expressions/script.js
@@ -0,0 +1,12 @@
+// Expressions: an expression is a piece of code that produces a value
+3 + 4;
+1991
+true && false && !false
+
+// Statement: statement is like a bigger piece of code that is executed and which does not produce a value on itself 
+if(23 > 10){
+  const str = '23 is bigger';
+}
+
+const me  = 'Junaid';
+console.log(`I'm ${2037 -1984} years old ${me}`);
