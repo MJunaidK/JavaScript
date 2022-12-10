55074028fc57c1fbe35ee05192362384f9e15227
MJunaidK <junaidcts@gmail.com>
2022-12-10 14:46:13 -0500

    Coding Challenge #2

---
+++ 02-Fundamentals-Part-2/Lecture 41 Coding Challenge #2/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const calcTip = function (billValue){
+  if (billValue >= 50 && billValue <=  300){
+    return billValue * 0.15
+  }else{
+    return billValue * 0.20
+  }
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
+
+/*
+Instructor solution
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+*/
+
