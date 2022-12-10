3b02a2052744aed1133a6ba06540f06d31c428c1
MJunaidK <junaidcts@gmail.com>
2022-12-10 16:11:24 -0500

    Introduction to objetcs

---
+++ 02-Fundamentals-Part-2/Lecture 42 Introduction to Objects/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const junaidArray = [
+  'Mohammad Junaid',
+  'Khan',
+  2022-1984,
+  'Architect',
+  ['I', 'Me', 'MySelf']
+];
+
+const junaidObj = {
+  firstName: 'Mohammad Junaid',
+  lastName: 'Khan',
+  age: 2022-1984,
+  job: 'Architect',
+  friends: ['I', 'Me', 'MySelf']
+}
+
+console.log(junaidArray);
+console.log(junaidObj);
