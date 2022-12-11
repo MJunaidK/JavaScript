cc41a7dffed97d22addb816250c7362cfe3cbf95
MJunaidK <junaidcts@gmail.com>
2022-12-11 10:52:47 -0500

    Iteration for loop

---
+++ 02-Fundamentals-Part-2/Lecture 46 Iteration The for Loop/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*console.log(`Lifting weights repetition 1 ⚡`);
+console.log(`Lifting weights repetition 2 ⚡`);
+console.log(`Lifting weights repetition 3 ⚡`);
+console.log(`Lifting weights repetition 4 ⚡`);
+console.log(`Lifting weights repetition 5 ⚡`);
+console.log(`Lifting weights repetition 6 ⚡`);
+console.log(`Lifting weights repetition 7 ⚡`);
+console.log(`Lifting weights repetition 8 ⚡`);
+console.log(`Lifting weights repetition 9 ⚡`);
+console.log(`Lifting weights repetition 10 ⚡`);
+*/
+
+// for loops keep running while condition is TRUE
+for(let rep = 1; rep <=10; rep ++ ){
+  console.log(`Lifting weights repetition ${rep} ⚡`);
+}
+
+/*
+Instructor code
+// Iteration: The for Loop
+
+// console.log('Lifting weights repetition 1 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 2 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 3 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 4 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 5 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 6 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 7 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 8 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 9 ðŸ‹ï¸â€â™€ï¸');
+// console.log('Lifting weights repetition 10 ðŸ‹ï¸â€â™€ï¸');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights repetition ${rep} ðŸ‹ï¸â€â™€ï¸`);
+}
+*/
