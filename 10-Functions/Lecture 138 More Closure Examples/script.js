2d8a2d65ae0436a0cff468e3dfdc76a968b57537
MJunaidK <junaidcts@gmail.com>
2023-01-29 16:41:38 -0500

    More closure examples

---
+++ 10-Functions/Lecture 138 More Closure Examples/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Example 1
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g(); // f will be assigned the inner function
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+// closure does in fact have priority over the scope chain.
+
+const perGroup = 1000;
+boardPassengers(180, 3);
