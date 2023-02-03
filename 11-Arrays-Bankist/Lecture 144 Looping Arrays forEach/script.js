68b40f6681ac5c6625d6daed30ef53b3e504dbe1
MJunaidK <junaidcts@gmail.com>
2023-02-02 21:23:31 -0500

    Looping Arrays forEach

---
+++ 11-Arrays-Bankist/Lecture 144 Looping Arrays forEach/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for of loop
+console.log('----FOR OF------');
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// for of with index
+console.log('----FOR OF INDEX------');
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// for each loop
+console.log('----FOREACH------');
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// for each with index
+console.log('----FOREACH INDEX------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
