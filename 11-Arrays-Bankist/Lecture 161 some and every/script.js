eaf70d0b43885cb5daab95dc86899ddd6e945c74
MJunaidK <junaidcts@gmail.com>
2023-02-08 11:38:02 -0500

    some and every method

--- 11-Arrays-Bankist/Lecture 161 some and every/script.js
+++ 11-Arrays-Bankist/Lecture 161 some and every/script.js
@@ -181,8 +181,6 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
-// Lecture 160 starts
-
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
@@ -205,7 +203,21 @@ btnClose.addEventListener('click', function (e) {
   inputCloseUsername.value = inputClosePin.value = '';
 });
 
-// Lecture 160 ends
+// Lecture 161 starts
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+// Lecture 161 ends
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const deposits = movements.filter(function (mov) {
@@ -282,3 +294,25 @@ console.log(firstWithdrawal);
 
 const account = accounts.find(acc => acc.owner === 'Salma Khan');
 console.log(account);
+
+// Lecture 161 starts
+
+// includes - testing for equality
+console.log(movements);
+console.log(movements.includes(-130));
+
+//some - test for a condition
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// every -  all of the elements in the array satisfy the condition that we pass in.
+
+console.log(movements.every(mov => mov > 0));
+console.log(accounts.at(3).movements.every(mov => mov > 0));
+
+//  Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+// Lecture 161 ends
