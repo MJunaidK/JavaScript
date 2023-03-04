7b447536e615580b9ced04e9fd459e560238eab3
MJunaidK <junaidcts@gmail.com>
2023-03-04 18:04:06 -0500

    Encapsulation protected properties and methods

--- 14-OOP/Lecture 223 Encapsulation Protected Properties and Methods/script.js
+++ 14-OOP/Lecture 223 Encapsulation Protected Properties and Methods/script.js
@@ -4,26 +4,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // Protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log('Loan Approved!');
     }
@@ -36,7 +42,8 @@ const acct1 = new Account('Junaid', 'USD', 1111);
 acct1.deposit(200);
 acct1.withdraw(140);
 acct1.requestLoan(1000);
-acct1.approveLoan(1000);
+//acct1.approveLoan(1000);
+console.log(acct1.getMovements());
 
 console.log(acct1);
-console.log(acct1.pin);
+console.log(acct1._pin);
