4f347ea35215e4446fabdf4d6446363685623b9e
MJunaidK <junaidcts@gmail.com>
2023-03-26 19:13:33 -0400

    Implementing error and success messages

---
+++ 18-forkify/Lecture 295 Implementing Error and Success Messages/src/js/controller.js
@@ -0,0 +1,33 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable'; // polyfilling everything else.
+import 'regenerator-runtime/runtime'; // polyfilling async/await.
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
