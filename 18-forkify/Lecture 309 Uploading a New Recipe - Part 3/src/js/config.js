7d4fd25859bcf1d4b005461e3474fa070428ddd4
MJunaidK <junaidcts@gmail.com>
2023-03-31 18:38:37 -0400

    Upload recipe part -3

---
+++ 18-forkify/Lecture 309 Uploading a New Recipe - Part 3/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0e567e92-cb3f-47bc-a5e2-c4fc02c47922';
+export const MODAL_CLOSE_SEC = 2.5;
